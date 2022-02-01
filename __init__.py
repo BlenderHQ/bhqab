841b165a197b8a3c3bc9971556a216ab22d7523f
Ivan Perevala <ivan95perevala@gmail.com>
2022-02-01 22:32:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,4 +14,96 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-print(__package__)
+from typing import Iterable
+
+if "bpy" in locals():
+    from importlib import reload
+
+    del reload
+else:
+    pass
+
+import bpy
+
+ADDON = __import__(__package__.split('.')[0])
+
+BL_INFO = getattr(ADDON, "bl_info", None)
+assert(BL_INFO is not None)
+
+assert("name" in BL_INFO)
+assert(isinstance(BL_INFO["name"], str))
+
+assert("blender" in BL_INFO)
+assert("version" in BL_INFO)
+assert(isinstance(BL_INFO["blender"], tuple))
+assert(isinstance(BL_INFO["version"], tuple))
+assert(len(BL_INFO["blender"]) == len(BL_INFO["version"]) == 3)
+assert(BL_INFO["blender"] <= BL_INFO["version"])
+
+if BL_INFO["blender"] < (3, 0, 0):
+    # https://developer.blender.org/T85675
+    if bpy.app.version < (3, 0, 0):
+        assert("wiki_url" in BL_INFO)
+        assert(isinstance(BL_INFO["wiki_url"], str))
+    else:
+        assert("doc_url" in BL_INFO)
+        assert(isinstance(BL_INFO["doc_url"], str))
+
+
+def addon_name() -> str:
+    return BL_INFO["name"]
+
+
+def addon_doc_url() -> str:
+    if bpy.app.version < (3, 0, 0):
+        return BL_INFO["wiki_url"]
+    else:
+        return BL_INFO["doc_url"]
+
+
+def earliest_tested_version() -> tuple:
+    return BL_INFO["blender"]
+
+
+def latest_tested_version() -> tuple:
+    return BL_INFO["version"]
+
+
+def version_string(ver: Iterable) -> str:
+    return '.'.join((str(_) for _ in ver))
+
+
+def register_helper(pref_cls: bpy.types.AddonPreferences):
+    assert(isinstance(pref_cls, bpy.types.AddonPreferences))
+
+    def wrapper(func):
+        earliest_tested = earliest_tested_version()
+        latest_tested = latest_tested_version()
+
+        if bpy.app.version < earliest_tested:
+            bpy.utils.register_class(pref_cls)
+            print(
+                "{0} WARNING: Current Blender version ({1}) is less than older tested ({2}). "
+                "Registered only addon user preferences, which warn user about that.\n"
+                "Please, visit the addon documentation:\n{3}".format(
+                    addon_name(), bpy.app.version_string, version_string(earliest_tested), addon_doc_url()
+                )
+            )
+            return
+        elif bpy.app.version > latest_tested:
+            print(
+                "{0} WARNING: Current Blender version ({1}) is greater than latest tested ({2}).\n"
+                "Please, visit the addon documentation:\n{3}".format(
+                    addon_name(), bpy.app.version_string, version_string(latest_tested), addon_doc_url()
+                )
+            )
+
+        func()
+
+    return wrapper
+
+
+def unregister_helper():
+    def wrapper(func):
+        func()
+    return wrapper
